832e3f7cdfeb9053163338dea61f34f7d975856d
fanuel <fanueln@stud.ntnu.no>
2022-03-06 20:37:58 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module no.ntnu.idatt1002.k204.tasystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens no.ntnu.idatt1002.k204.tasystem to javafx.fxml;
+    exports no.ntnu.idatt1002.k204.tasystem;
+    exports no.ntnu.idatt1002.k204.tasystem.controller;
+    opens no.ntnu.idatt1002.k204.tasystem.controller to javafx.fxml;
+}
---
+++ src/main/java/no/ntnu/idatt1002/k204/tasystem/controller/HelloController.java
@@ -0,0 +1,14 @@
+package no.ntnu.idatt1002.k204.tasystem.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
