2998cc571ff78b7a4b1ca1b91b806ce97db98cfc
Karo Mahmoud <karofm@stud.ntnu.no>
2022-03-16 09:27:47 +0100

    Did changes to attributes, constructor and methods in Tournament-class.

--- src/main/java/no/ntnu/idatt1002/k204/tasystem/Tournament.java
+++ src/main/java/no/ntnu/idatt1002/k204/tasystem/Tournament.java
@@ -1,15 +1,23 @@
 package no.ntnu.idatt1002.k204.tasystem;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Tournament {
     private String name;
+    private String rankRequirement;
+    private boolean hasGroupStage;
+    private LocalDateTime dateTime;
     private ArrayList<Team> teams;
     private boolean isActive;
     private Team winner;
 
-    public Tournament(String name, ArrayList<Team> teams) {
+    public Tournament(String name, String rankRequirement,boolean hasGroupStage,LocalDateTime dateTime, ArrayList<Team> teams) {
         this.name = name;
+        this.rankRequirement=rankRequirement;
+        this.hasGroupStage=hasGroupStage;
+        this.dateTime=dateTime;
         this.teams = teams;
         this.isActive = isActive;
         this.winner = winner;
