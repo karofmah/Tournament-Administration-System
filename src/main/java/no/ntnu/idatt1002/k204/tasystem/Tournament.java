661a4b605443f1139ce3dc7e58fac6c80539cf92
Karo Mahmoud <karofm@stud.ntnu.no>
2022-03-16 10:53:04 +0100

    Added and changed methods in Tournament-class. Implemented java doc.

--- src/main/java/no/ntnu/idatt1002/k204/tasystem/Tournament.java
+++ src/main/java/no/ntnu/idatt1002/k204/tasystem/Tournament.java
@@ -12,9 +12,23 @@ public class Tournament {
     private boolean isActive;
     private Team winner;
 
-    public Tournament(String name, String rankRequirement,boolean hasGroupStage,LocalDateTime dateTime) {
+    /**
+     * Creates constructor for Tournament
+     * @param name String that represents the name of the tournament, can not be blank
+     * @param rankRequirement String that represents the rank requirement for the tournament, Unranked if blank
+     * @param hasGroupStage Boolean that represents wheteher group stage will be included or not
+     * @param dateTime LocalDateTime that represents the local date and time of the tournament
+     * @throws IllegalArgumentException
+     */
+    public Tournament(String name, String rankRequirement,boolean hasGroupStage,LocalDateTime dateTime) throws IllegalArgumentException {
         this.name = name;
+        if(name.isBlank()) {
+            throw new IllegalArgumentException("Name can not be blank");
+        }
         this.rankRequirement=rankRequirement;
+        if(rankRequirement.isBlank()) {
+            rankRequirement="Unranked";
+        }
         this.hasGroupStage=hasGroupStage;
         this.dateTime=dateTime;
         this.teams = new ArrayList<Team>();
@@ -22,28 +36,51 @@ public class Tournament {
         this.winner = null;
     }
 
+    /**
+     * Returns name of the tournament
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns teams in the tournament
+     * @return teams
+     */
     public ArrayList<Team> getTeams() {
         return teams;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean isActive() {
         return isActive;
     }
 
+    /**
+     *
+     * @return
+     */
     public Team getWinner() {
         return winner;
     }
 
+    /**
+     *
+     * @param active
+     */
     public void setActive(boolean active) {
         isActive = active;
     }
-    public void setWinner(Team winner){
-        this.winner=winner;
-    }
+
+    /**
+     *
+     * @param winner
+     */
+    public void setWinner(Team winner){this.winner=winner;}
 
     public boolean addTeam(Team team){
         if(!teams.contains(team)){
@@ -53,12 +90,33 @@ public class Tournament {
             return false;
         }
     }
+
+    /**
+     *
+     */
     public void startTournament() {
         setActive(true);
     }
 
+    /**
+     *
+     * @param winner
+     */
     public void finishTournament(Team winner){
         setActive(false);
         setWinner(winner);
+    }
+
+    @Override
+    public String toString() {
+        return "Tournament{" +
+                "name='" + name + '\'' +
+                ", rankRequirement='" + rankRequirement + '\'' +
+                ", hasGroupStage=" + hasGroupStage +
+                ", dateTime=" + dateTime +
+                ", teams=" + teams +
+                ", isActive=" + isActive +
+                ", winner=" + winner +
+                '}';
     }
 }
