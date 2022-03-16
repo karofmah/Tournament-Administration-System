5cf57cb85a2d8dab6ba155be38e75433c8b21a14
Emil Orvik Olsson~ <emiloo@stud.ntnu.no>
2022-03-16 11:16:10 +0100

    TournamentRegisterTestKAOS

--- src/test/java/tests/TournamentRegisterTest.java
+++ src/test/java/tests/TournamentRegisterTest.java
@@ -1,18 +1,35 @@
 package tests;
+import no.ntnu.idatt1002.k204.tasystem.Player;
+import no.ntnu.idatt1002.k204.tasystem.Team;
+import no.ntnu.idatt1002.k204.tasystem.Tournament;
 import no.ntnu.idatt1002.k204.tasystem.TournamentRegister;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
 
 public class TournamentRegisterTest {
+    TournamentRegister register = new TournamentRegister();
+    void TestData(){
+        Player player1 = new Player("Sjokoladepudden", "Silver");
+        Player player2 = new Player("Phase", "Iron");
+        ArrayList<Player> playerlist = new ArrayList<>();
+        playerlist.add(player1);
+        playerlist.add(player2);
+        Team team1 = new Team(playerlist,"GeirSittLag",0);
+
+    }
+
 
     @Test
-    void addTournament(){
-        TournamentRegister register = new TournamentRegister();
+    public void addTournament(){
+        TestData();
+        Tournament tournament = new Tournament("SummonersRift","Silver",false, LocalDateTime.of(2022, Month.APRIL,28,14,00,00));
         register.addTournament("SummonersRift","Silver",false, LocalDateTime.of(2022, Month.APRIL,28,14,00,00));
-        Assertions.assertEquals(register.getTournaments().get(0).getName(),"SummonersRift");
+        System.out.println(register.toString());
+        Assertions.assertTrue(register.getTournaments().get(0)==(tournament));
         register.addTournament("Tirsdagsturnering","Unranked",false, LocalDateTime.of(2022, Month.APRIL,18,14,00,00));
         register.addTournament("Onsdagsturnering","Silver",true, LocalDateTime.of(2022, Month.MARCH,28,14,00,00));
         System.out.println(register.getTournaments());
