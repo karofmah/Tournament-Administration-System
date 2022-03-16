bf06c3301f524400faf46ba7a5d63135c341c1cd
Emil Orvik Olsson~ <emiloo@stud.ntnu.no>
2022-03-16 10:02:04 +0100

    Removed junit requirement from module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module no.ntnu.idatt1002.k204.tasystem {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.junit.jupiter.api;
+
 
 
     opens no.ntnu.idatt1002.k204.tasystem to javafx.fxml;
