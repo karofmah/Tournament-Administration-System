eddbbc34c93fcce4b1af29573712e8f2a5364cc2
Fredrik Ruud <fbruud@gmail.com>
2022-03-21 12:19:48 +0100

    Added a basic Knockout Stage View with an empty controller

--- src/main/java/no/ntnu/idatt1002/k204/tasystem/controller/KnockoutStageController.java
+++ src/main/java/no/ntnu/idatt1002/k204/tasystem/controller/KnockoutStageController.java
@@ -1,6 +1,54 @@
 package no.ntnu.idatt1002.k204.tasystem.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.TextFlow;
+
 public class KnockoutStageController {
 
+    @FXML
+    private TextFlow team1;
+
+    @FXML
+    private TextFlow team2;
+
+    @FXML
+    private TextFlow team3;
+
+    @FXML
+    private TextFlow team4;
+
+    @FXML
+    private TextFlow team5;
+
+    @FXML
+    private TextFlow team6;
+
+    @FXML
+    private TextFlow team7;
+
+    @FXML
+    private TextFlow team8;
+
+    @FXML
+    private TextFlow quarterFinalTeam1;
+
+    @FXML
+    private TextFlow quarterFinalTeam2;
+
+    @FXML
+    private TextFlow quarterFinalTeam3;
+
+    @FXML
+    private TextFlow quarterFinalTeam4;
+
+    @FXML
+    private TextFlow semiFinalTeam1;
+
+    @FXML
+    private TextFlow semiFinalTeam2;
+
+    @FXML
+    private TextFlow winnerTeam;
 
 }
+
