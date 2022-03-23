a0175bbb3f124f79f5c5ce2971d5c8923840e554
Edvard Sørby <edvarso@stud.ntnu.no>
2022-03-23 15:31:29 +0100

    Added the ability to click on teams in the team register to view all players and their ranks.

---
+++ src/main/java/no/ntnu/idatt1002/k204/tasystem/controller/SelectedTeamController.java
@@ -0,0 +1,83 @@
+package no.ntnu.idatt1002.k204.tasystem.controller;
+
+/**
+ * Sample Skeleton for 'selectedTeamView.fxml' Controller Class
+ */
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import no.ntnu.idatt1002.k204.tasystem.Application;
+import no.ntnu.idatt1002.k204.tasystem.model.Player;
+import no.ntnu.idatt1002.k204.tasystem.model.Team;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SelectedTeamController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="backBtn"
+    private Button backBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="logOutBtn"
+    private Button logOutBtn; // Value injected by FXMLLoader
+
+    @FXML
+    private Label selectedText;
+
+    @FXML
+    private TableColumn<Player, String> playerNameCol;
+
+    @FXML
+    private TableColumn<Player, String> playerRankCol;
+
+    @FXML
+    private TableView<Player> playersTableView;
+
+    private Team selectedTeam;
+
+    @FXML
+    void backBtnClicked(ActionEvent event) {
+        try {
+            Application.changeScene("teamsView.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void logOutBtnClicked(ActionEvent event) {
+        try {
+            Application.logout();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Adds data to the table.
+     * @param team the selected team
+     */
+    public void initData(Team team){
+        selectedTeam = team;
+        selectedText.setText(team.getTeamName());
+
+        playerNameCol.setCellValueFactory(new PropertyValueFactory<>("gamertag"));
+        playerRankCol.setCellValueFactory(new PropertyValueFactory<>("rank"));
+
+        for (Player player : team.getPlayers()) {
+            playersTableView.getItems().add(player);
+        }
+    }
+}
--- src/main/java/no/ntnu/idatt1002/k204/tasystem/controller/TeamsController.java
+++ src/main/java/no/ntnu/idatt1002/k204/tasystem/controller/TeamsController.java
@@ -3,15 +3,21 @@ package no.ntnu.idatt1002.k204.tasystem.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import no.ntnu.idatt1002.k204.tasystem.Application;
 import no.ntnu.idatt1002.k204.tasystem.dao.TeamDAO;
 import no.ntnu.idatt1002.k204.tasystem.model.Team;
 import no.ntnu.idatt1002.k204.tasystem.model.TeamRegister;
+import no.ntnu.idatt1002.k204.tasystem.model.Tournament;
 
 import java.io.IOException;
 import java.net.URL;
@@ -57,6 +63,63 @@ public class TeamsController implements Initializable {
         this.teamObservableList = FXCollections.observableArrayList(this.teamRegister.getTeams());
         this.teamsTableView.setItems(this.teamObservableList);
 
+        handleTeamSelection();
+    }
+
+    /**
+     * Change scene when a team is selected.
+     *
+     * Listen on hovered row in table and check if there is a team
+     * If there is a team allow clicking and change scene.
+     *
+     * Also changes cursor type to try and communicate with user that current row
+     * can be clicked.
+     *
+     */
+
+    private void handleTeamSelection() {
+        teamsTableView.setRowFactory(table -> {
+            TableRow<Team> row = new TableRow<>();
+
+            row.hoverProperty().addListener(observable -> {//Listen for hover on row
+                Team team = row.getItem();
+                if (row.isHover() && team != null) {
+                    row.setOnMouseEntered(mouseEvent1 -> {//Listen when mouse is hovered over a row
+                        teamsTableView.setCursor(Cursor.HAND);//Change courser
+                        row.setOnMouseClicked(mouseEvent2 -> { //Listen for click event
+                            try {
+                                changeToSelectedTeamView(team); //Change scene
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+
+                        });
+                    });
+                } else {
+                    row.setOnMouseEntered(mouseEvent -> { //Default cursor when row is empty
+                        teamsTableView.setCursor(Cursor.DEFAULT);
+                    });
+                }
+            });
+
+            return row;
+        });
+    }
+
+    private void changeToSelectedTeamView(Team team) throws IOException {
+        try {
+            URL fxmlLocation = getClass().getResource("/no/ntnu/idatt1002/k204/tasystem/selectedTeamView.fxml");
+            FXMLLoader loader = new FXMLLoader(fxmlLocation);
+            Parent FrontPageParent = loader.load();
+            SelectedTeamController controller = loader.getController();
+            controller.initData(team);
+            Stage stage = Application.stage;
+            stage.getScene().setRoot(FrontPageParent);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
